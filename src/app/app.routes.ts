9af0a9e49c60881e39c76b049f0d46ec59eb9fbd
Taimander <taimander01@gmail.com>
2025-01-20 18:04:19 -0600

    change routes structure and add login screen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,15 @@ import { Routes } from '@angular/router';
 import { ProductsIndexComponent } from './views/products/products-index/products-index.component';
 import { ViewProductComponent } from './views/products/view-product/view-product.component';
 import { LoginComponent } from './views/login/login.component';
+import { NavlayoutComponent } from './layout/navlayout/navlayout.component';
 
 export const routes: Routes = [
     {path: '', redirectTo: 'products', pathMatch: 'full'},
-    {path: 'products', component: ProductsIndexComponent},
-    {path: 'products/:id', component: ViewProductComponent},
+    {path: '', component: NavlayoutComponent, children: [
+        {path: 'products', component: ProductsIndexComponent},
+        {path: 'products/:id', component: ViewProductComponent},
+        
+    ]},
     {path: 'login', component: LoginComponent},
     {path: '**', redirectTo: 'products'}
 ];
